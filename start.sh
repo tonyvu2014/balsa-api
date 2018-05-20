5eefb59345070c084a00ee2215cd21d7cc54954e
Tony Vu <tonyvu2008@gmail.com>
2018-05-20 22:10:04 +1000

    Add start and stop script

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source venv/bin/activate
+pip install -r requirements.txt
+cd balsa && gunicorn -b 127.0.0.1:8000 -k gevent -pvar/run/gunicorn.pid --error-logfile /var/log/balsa/error.log -D balsa.wsgi 
