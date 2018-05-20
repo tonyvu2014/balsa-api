36fda408e1572cf0c919a1a778db33484557ae22
Tony Vu <tonyvu2008@gmail.com>
2018-05-20 22:11:07 +1000

    Add space in command

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 source venv/bin/activate
 pip install -r requirements.txt
-cd balsa && gunicorn -b 127.0.0.1:8000 -k gevent -pvar/run/gunicorn.pid --error-logfile /var/log/balsa/error.log -D balsa.wsgi 
+cd balsa && gunicorn -b 127.0.0.1:8000 -k gevent -p var/run/gunicorn.pid --error-logfile /var/log/balsa/error.log -D balsa.wsgi 
