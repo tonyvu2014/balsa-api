f18a64309684475edc0ac7baadb84ff122e9b58c
Tony Vu <tonyvu2008@gmail.com>
2018-05-20 22:46:10 +1000

    allow hosts

--- balsa/balsa/settings.py
+++ balsa/balsa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vz70h%lb0kuum!x0)j5-#$gh5fuj5cla&f#)zl=+o-v026#jkl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
